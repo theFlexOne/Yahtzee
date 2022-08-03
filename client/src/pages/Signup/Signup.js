8345a657a980390577f47b0ee296a1c3dd3e9c6f
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2022-08-02 21:27:15 -0500

    pushing up to Heroku

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -13,48 +13,58 @@ const initialValues = {
 };
 
 const Signup = () => {
-  const formik = useFormik({ initialValues });
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  const formik = useFormik({ initialValues, onSubmit });
 
   const { user } = useUser();
   const navigate = useNavigate();
 
   if (user) return navigate("/");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formik.values);
-  };
-
   return (
     <div className="signup">
-      <Form id="signupForm" onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="create a unique username"
-          value={formik.values.username}
-          onChange={formik.handleChange}
-        />
-        <label htmlFor="username">Password</label>
-        <input
-          type="text"
-          id="password"
-          name="password"
-          placeholder="must be at least 7 characters"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-        />
-        <label htmlFor="username">Re-enter Password</label>
-        <input
-          type="text"
-          id="passwordConfirmation"
-          name="passwordConfirmation"
-          placeholder="confirm password"
-          value={formik.values.passwordConfirmation}
-          onChange={formik.handleChange}
-        />
+      <Form id="signupForm" onSubmit={formik.handleSubmit}>
+        <div className="input-container">
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="create a unique username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+            />
+          </div>
+        </div>
+        <div className="input-container">
+          <div className="input-wrapper">
+            <label htmlFor="username">Password</label>
+            <input
+              type="text"
+              id="password"
+              name="password"
+              placeholder="must be at least 7 characters"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+            />
+          </div>
+        </div>
+        <div className="input-container">
+          <div className="input-wrapper">
+            <label htmlFor="username">Re-enter Password</label>
+            <input
+              type="text"
+              id="passwordConfirmation"
+              name="passwordConfirmation"
+              placeholder="confirm password"
+              value={formik.values.passwordConfirmation}
+              onChange={formik.handleChange}
+            />
+          </div>
+        </div>
         <YahtzeeButton data-styling="secondary">Submit</YahtzeeButton>
       </Form>
     </div>
